a113537a1ef846da525473f379cd6ca144ca21cd
Sander Dijkman <sdijkman2008@gmail.com>
2019-11-07 08:51:38 +0100

    review can be added but is not yet visible

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,27 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy ]
 
   def index
     @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
   end
 
   def update
@@ -10,5 +30,19 @@ class RestaurantsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @restaurant.destroy
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  resources :reviews
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :create ]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
